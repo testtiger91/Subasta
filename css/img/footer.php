bc705581b503ed953dc34fcee314c250224dbf16
testtiger91 <60692352+testtiger91@users.noreply.github.com>
2020-06-08 06:41:26 -0300

    First Commit

---
+++ css/img/footer.php
@@ -0,0 +1,48 @@
+<footer class="page-footer light-blue lighten-1">
+  <div class="container">
+    <div class="row">
+      <div class="col l5 s12">
+        <h5 class="white-text"><u>Ubicación</u></h5>
+        <p>Alvear 33, San Jose de Feliciano, Feliciano, Entre Ríos</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3441.738893361813!2d-58.75943224908919!3d-30.386771454154612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x944d35c098412e55%3A0xe71a5a9de8318ebd!2sAlvear%2C%20San%20Jose%20de%20Feliciano%2C%20Entre%20R%C3%ADos!5e0!3m2!1ses-419!2sar!4v1587595295957!5m2!1ses-419!2sar" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      </div>
+      <div class="col l4 s12">
+        <h5 class="white-text"><u>Contactos</u></h5>
+        <ul>
+          <li><a class="white-text" href="#!"><i class="material-icons prefix">mail_outline</i><br>CME.dr.LeitesHector@hotmail.com</a></li>
+          <li><a class="white-text" href="#!"><i class="material-icons prefix">phone</i><br>03458 15659442</a></li>
+          <li><a class="white-text" href="https://wa.link/cwfyb2"><img class="footer-img" src="img/whatsapp.png"><br>Tambien podes consultarnos por WhatsApp</a></li>
+        </ul>
+      </div>
+      <div class="col l3 s12">
+        <h5 class="white-text"><u>Redes</u></h5>
+        <ul>
+          <li><a class="white-text" href=""><img class="footer-img" src="img/facebook.png"><br>Facebook</a></li>
+          <li><a class="white-text" href=""><img class="footer-img" src="img/twitter.png"><br>Twitter</a></li>
+          <li><a class="white-text" href=""><img class="footer-img"  src="img/instagram.png"><br>Instagram</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- <div class="row right">
+      <div class="container">
+        <div class="col l3 s12"> -->
+          <!-- Element Showed -->
+          <!-- <a id="menu" class="waves-effect waves-light btn btn-floating" onclick="$('.tap-target').tapTarget('open')"><i class="material-icons">menu</i></a> -->
+
+          <!-- Tap Target Structure -->
+          <!-- <div class="tap-target" data-target="menu">
+            <div class="tap-target-content">
+              <h5>Title</h5>
+              <p>A bunch of text</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      <!-- <a class="orange-text text-lighten-3" href="#">Copyright y datos</a> -->
+    </div>
+  </div>
+</footer>
